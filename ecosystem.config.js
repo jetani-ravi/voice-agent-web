970668cf33bba3e25f9cb848564180644688db4d
mubashshir-cloudmate <mubashshir@cloudmatetechnologies.com>
2025-02-07 12:31:17 +0530

    fix: env staging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 /*
-    pm2 deploy ecosystem.config.js staging setup
-    pm2 deploy ecosystem.config.js staging update
+    pm2 deploy ecosystem.config.js production setup
+    pm2 deploy ecosystem.config.js production update
 
     pm2 deploy ecosystem.config.js dev update
 
@@ -9,24 +9,24 @@
 module.exports = {
     apps: [
       {
-        name: 'portal-voice-agent-stage',
+        name: 'portal-voice-agent-app',
         script: 'npm',
         args: 'start',
         env: {
-          NODE_ENV: 'staging',
+          NODE_ENV: 'production',
           PORT: 4000
         }
       }
     ],
     deploy: {
-      staging: {
+      production: {
         user: 'ubuntu',
         host: '98.80.59.215',
         ref: 'origin/main',
         repo: 'git@github.com:mubashshir-cloudmate/voice-agent-web.git',
         path: '/home/ubuntu/workspace/voice-agent-portal',
         'post-deploy':
-          'nvm use && /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pnpm i && /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pnpm build && /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pm2 reload ecosystem.config.js --env staging'
+          'nvm use && /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pnpm i && /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pnpm build && /home/ubuntu/.nvm/versions/node/v22.12.0/bin/pm2 reload ecosystem.config.js --env production'
       }
     }
   }
